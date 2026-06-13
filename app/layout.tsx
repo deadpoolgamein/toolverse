c2f418c6def2ffc4d1dea078123f8bba9338231a
Sandeep Singh <furryyt004@gmail.com>
2026-06-13 19:52:54 +0530

    AdSense code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; // AdSense script chalane ke liye zaroori hai
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+ });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -31,6 +32,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* --- YAHA PAR AAPKA REAL ADSENSE CODE AUTOMATIC SET HAI --- */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8180011709154612"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="min-h-full flex flex-col bg-zinc-950 text-white">
         {children}
       </body>
